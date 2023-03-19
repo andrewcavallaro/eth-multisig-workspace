c0e54a691d1287b54f87de00be0ab69e9177f407
Andrew C <andrew@meta-z.io>
2023-03-19 13:28:25 -0600

    info from bitgo's eth-multisig v4 as of March 19 2023, and the openzeppelin deps

---
+++ contracts/test/Fail.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.10;
+
+contract Fail {
+  fallback() external payable {
+    revert('Oops');
+  }
+
+  receive() external payable {
+    revert('Oops');
+  }
+}
---
+++ contracts/test/ForwarderTarget.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.10;
+
+import '../Forwarder.sol';
+
+// This is a test target for a Forwarder.
+// It contains a public function with a side-effect.
+contract ForwarderTarget {
+  uint256 public data;
+  event Received();
+
+  function setDataWithValue(uint256 d, bool b) external payable returns (bool) {
+    data = d;
+    return b;
+  }
+
+  function setData(uint256 d, bool b) external returns (bool) {
+    data = d;
+    return b;
+  }
+
+  function createForwarder() public {
+    new Forwarder();
+  }
+
+  /**
+   * Default function; Gets called when Ether is deposited or no function matches
+   */
+  fallback() external payable {
+    // accept unspendable balance
+    emit Received();
+  }
+
+  /**
+   * Plain Receive function; Gets called when Ether is deposited with no data
+   */
+  receive() external payable {
+    // accept unspendable balance
+    emit Received();
+  }
+}
